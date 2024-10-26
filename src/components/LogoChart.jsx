3bec5f7c7b71a46ccfc1378ffff42ccd7160bb88
Joaquín Cavitelli <75315685+JoaquinCaviltelli@users.noreply.github.com>
2024-10-26 10:28:27 -0300

    v19

--- src/components/LogoChart.jsx
+++ src/components/LogoChart.jsx
@@ -2,6 +2,7 @@ import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import { useState, useEffect } from "react";
+import "/src/assets/logoT.svg"
 
 ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
@@ -86,7 +87,8 @@ const LogoChart = () => {
     <div className="flex items-center justify-center mb-4 relative">
                 <div className="absolute inset-0 flex flex-col items-center justify-center cursor-pointer  rounded-full m-10">
                     
-                    <img src="/src/assets/logoT.svg" className="w-14" alt="" />
+                    <img src="src/assets/logoT.svg" className="w-14" alt="" />
+                    
                 </div>
                 
                 <Doughnut
